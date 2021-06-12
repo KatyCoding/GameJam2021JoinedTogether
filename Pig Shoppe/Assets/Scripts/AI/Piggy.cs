8b9e55b9a7159fdaf999587453480cee7a9d1b68
KatyCoding <50761497+KatyCoding@users.noreply.github.com>
2021-06-12 00:58:24 -0400

    piggy sit, piggy run, piggy zoom

--- Pig Shoppe/Assets/Scripts/AI/Piggy.cs
+++ Pig Shoppe/Assets/Scripts/AI/Piggy.cs
@@ -9,6 +9,24 @@ public class Piggy : MonoBehaviour, IGrabbable
     public AudioClip OnGrabbedAudio;
     public string OnGrabbedAnimationTrigger;
     public PositionConstraint harpoonGrabbedConstraint;
+
+    [Header("Pathfinding")]
+    public float maxSpeed = 2;
+    [Range(0, 1f)]
+    public float chanceToSit = .1f;
+    public LayerMask pigPathfindingLayer;
+    public float pigSitMinTime = 1;
+    public float pigSitMaxTime = 6;
+    
+    [SerializeField]
+    private Rigidbody pigsRigidbody;
+    #region private pathfinding fields
+    Vector3 acceleration = Vector3.zero;
+    Vector3 velocity = Vector3.zero;
+    float sitDownTimer = 0;
+    float sitDownTimeLength;
+    bool isSitting = false;
+    #endregion
     public void OnArrive()
     {
         harpoonGrabbedConstraint.RemoveSource(0);
@@ -22,6 +40,7 @@ public class Piggy : MonoBehaviour, IGrabbable
 
     public void OnGrabbed(Transform grabbingObject)
     {
+        maxSpeed = 0;
         //playAudio
         //triggeranimation
         ConstraintSource cs = new ConstraintSource();
@@ -33,7 +52,69 @@ public class Piggy : MonoBehaviour, IGrabbable
 
     public void WhileDragging()
     {
+
+    }
+    public void Awake()
+    {
+        acceleration = new Vector3(Random.value - .5f, 0, Random.value - .5f);
+    }
+    public void Update()
+    {
+
+    }
+    public void FixedUpdate()
+    {
+        HandlePathfinding();
+        this.transform.up = velocity.normalized;
     }
 
-    
+    protected virtual void HandlePathfinding()
+    {
+        pigsRigidbody.velocity = Vector3.zero;
+        float r = Random.Range(0f, 100f);
+        if(r < chanceToSit)
+        {
+            isSitting = true;
+            sitDownTimeLength = Random.Range(pigSitMinTime, pigSitMaxTime);
+            sitDownTimer = 0;
+            Debug.Log("Sitting");
+        }
+        if(isSitting && sitDownTimer < sitDownTimeLength)
+        {
+            velocity = Vector3.zero;
+            sitDownTimer += Time.fixedDeltaTime;
+            return;
+        }
+        isSitting = false;
+        var cols = Physics.OverlapSphere(this.transform.position, 3);
+        var pigsNearby = new List<Piggy>();
+        for (int i = 0; i < cols.Length; i++)
+        {
+            var pig = cols[i].GetComponent<Piggy>();
+            if (pig)
+            {
+                pigsNearby.Add(pig);
+                acceleration += -(pig.transform.position - this.transform.position).normalized * .5f;
+            }
+        }
+        acceleration += new Vector3(Random.value - .5f, 0, Random.value - .5f) * .4f;
+        acceleration = acceleration.normalized;
+        velocity += acceleration * Time.fixedDeltaTime;
+        if (velocity.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            velocity = velocity.normalized * maxSpeed;
+        }
+        velocity.y = 0;
+        transform.position += velocity * Time.fixedDeltaTime;
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (1<<collision.collider.gameObject.layer == pigPathfindingLayer.value)
+        {
+            acceleration = Vector3.zero;
+            var newX = collision.GetContact(0).normal.x;
+            var newZ = collision.GetContact(0).normal.z;
+            velocity = new Vector3(newX, 0, newZ);
+        }
+    }
 }
--- Pig Shoppe/Assets/Scripts/Harpoon/HarpoonTurnControls.cs
+++ Pig Shoppe/Assets/Scripts/Harpoon/HarpoonTurnControls.cs
@@ -8,21 +8,42 @@ public class HarpoonTurnControls : MonoBehaviour
     public Camera gameCam;
     [SerializeField]
     private HarpoonShoot shootControls;
+    private Vector3 prevMouse = Vector3.zero;
+    public void Start()
+    {
+        prevMouse = Input.mousePosition;
+    }
     public void FixedUpdate()
     {
         if (shootControls.isFired)
             return;
-        var positionToFace = Vector3.zero;
-        RaycastHit hit;
-        if (Physics.Raycast(gameCam.transform.position,gameCam.ScreenPointToRay(Input.mousePosition).direction, out hit, 50))//add layer for ground only
+        //new Quaternion(0f, Mathf.Sin(angleXDelta / 2f), 0, Mathf.Cos(angleXDelta / 2f)).normalized;
+
+        var angleXDelta = (Input.mousePosition - prevMouse).x * .003f;
+        var angleYDelta = (Input.mousePosition - prevMouse).y * .003f;
+        prevMouse = Input.mousePosition;
+        this.transform.rotation *= new Quaternion(Mathf.Sin(-angleYDelta / 2f), 0f, 0f, Mathf.Cos(-angleYDelta / 2f));
+        this.transform.rotation = new Quaternion(0f, Mathf.Sin(angleXDelta / 2f), 0, Mathf.Cos(angleXDelta / 2f)).normalized * this.transform.rotation; //new Quaternion(Mathf.Sin(angleXDelta / 2f) * this.transform.up.x, Mathf.Sin(angleXDelta / 2f) * this.transform.up.y, Mathf.Sin(angleXDelta / 2f) * this.transform.up.z, Mathf.Cos(angleXDelta / 2f)).normalized * this.transform.rotation;
+        if (Vector3.Dot(this.transform.forward,new Vector3(this.transform.forward.x,0,this.transform.forward.z).normalized) < .5f)
         {
-            positionToFace = new Vector3(hit.point.x, 0, hit.point.z);
+            //this.transform.rotation *= new Quaternion(Mathf.Sin(-angleYDelta / 2f), 0f, 0f, Mathf.Cos(-angleYDelta / 2f));
+
         }
-        else
-        {
-            return;
-        }
-        var targetRotation = Quaternion.LookRotation(positionToFace.normalized, Vector3.up);
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.fixedDeltaTime * rotationMultiplier);
+
+        #region Top Down
+
+        //var positionToFace = Vector3.zero;
+        //RaycastHit hit;
+        //if (Physics.Raycast(gameCam.transform.position,gameCam.ScreenPointToRay(Input.mousePosition).direction, out hit, 50))//add layer for ground only
+        //{
+        //    positionToFace = new Vector3(hit.point.x, 0, hit.point.z);
+        //}
+        //else
+        //{
+        //    return;
+        //}
+        //var targetRotation = Quaternion.LookRotation(positionToFace.normalized, Vector3.up);
+        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.fixedDeltaTime * rotationMultiplier);
+        #endregion
     }
 }
